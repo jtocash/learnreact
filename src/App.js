03aff62ebac96804f1e999bf8dd508f954cdbaed
jacktocash <jacktocash@icloud.com>
2024-05-09 10:40:07 -0400

    Testing Git.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App"> 
+  <button onClick = { () => { setCount(count + 1)}}>increase</button>
+  <button onClick = { () => { setCount(count - 1)}}>decrease</button>
+  <button onClick = { () => { setCount(0)}}>set zero</button>
+  {count}
+   </div>
+ 
+ 
   );
+
 }
+
+
 
 export default App;
